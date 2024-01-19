2741b7ab3500f378feaaaa8b4d0aeabf0fb48e10
Pranav Kumar <engineersempire0204@gmail.com>
2024-01-19 20:43:46 +0530

    add notes

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,57 @@
-const fn = require("./notes")
+const chalk = require("chalk");
+const yargs = require("yargs");
+const { getNotes, addNotes, removeNotes } = require("./notes");
 
+// create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: (argv) => {
+    addNotes(argv.title, argv.body);
+  },
+});
+//create remove command
+yargs.command({
+  command: "remove",
+  describe: "remove a note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: (argv) => {
+    removeNotes(argv.title);
+  },
+});
+// create read command
+yargs.command({
+  command: "read",
+  describe: "read a note",
+  handler: () => {
+    console.log("Reading a note");
+  },
+});
+//create list command
+yargs.command({
+  command: "list",
+  describe: "list your notes",
+  handler: () => {
+    console.log("Listing all notes");
+  },
+});
 
-console.log(fn.getNotes());
+yargs.parse();
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,3 +1,51 @@
-module.exports.getNotes = () => {
-    return "Your notes..."
-}
+const fs = require("fs");
+const chalk = require("chalk");
+
+const getNotes = () => {
+  return "Your notes...";
+};
+
+const addNotes = (title, body) => {
+  const notes = loadNotes();
+  if (duplicateNote(title)) {
+    console.log(chalk.red("Title name already exit!\nTry new Title name"));
+    return;
+  }
+  fs.writeFileSync(
+    "./notes.json",
+    JSON.stringify([...notes, { title: title, body: body }])
+  );
+  console.log("Successfully note added!");
+};
+const duplicateNote = (title) => {
+  try {
+    const notesJson = fs.readFileSync("./notes.json", "utf-8");
+    const notes = JSON.parse(notesJson);
+    const index = notes.findIndex((e) => e.title === title);
+    if (index === -1) return false;
+    return true;
+  } catch {
+    return false;
+  }
+};
+
+const loadNotes = () => {
+  try {
+    const notes = fs.readFileSync("./notes.json", "utf-8");
+    return JSON.parse(notes);
+  } catch {
+    return [];
+  }
+};
+const removeNotes = (title) => {
+  try {
+    const notesJson = fs.readFileSync("./notes.json", "utf-8");
+    const notes = JSON.parse(notesJson);
+    const index = notes.findIndex((e) => e.title === title);
+    if (index === -1) console.log("Title name not found!");
+    else notes.splice(index, 1);
+  } catch {
+    console.log("Note not found");
+  }
+};
+module.exports = { getNotes, addNotes, removeNotes };
